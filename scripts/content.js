b8fb13e01cee20f4b4e655e30cb41ce30b6393d1
afeiship <1290657123@qq.com>
2020-01-02 18:07:57 +0800

    fix: context

--- scripts/content.js
+++ scripts/content.js
@@ -5,4 +5,7 @@ $(document).ready(function() {
   $("#idSIButton9").prop("disabled", true);
   $("#idSIButton9").css("background", "#f00");
   console.info("[log]: has modifyed. 🐶");
+
+  // document.querySelector('[ng-disabled="list.length>2"]')
+  // document.querySelector('[ng-disabled=gridSelectAllDisabled]')
 });
