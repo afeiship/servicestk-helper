a9d2a02ef22a8347c1ecf56d53667e8c5101f07c
afeiship <1290657123@qq.com>
2020-01-11 20:10:09 +0800

    fix: update

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,7 @@
-console.log("content ready!!");
-
 $(document).ready(function() {
-  console.log($("#idSIButton9"));
-  $("#idSIButton9").prop("disabled", true);
-  $("#idSIButton9").css("background", "#f00");
-  console.info("[log]: has modifyed. 🐶");
-
-  // document.querySelector('[ng-disabled="list.length>2"]')
-  // document.querySelector('[ng-disabled=gridSelectAllDisabled]')
+  var el = document.querySelector('[ng-click="gridSelectAll()"]');
+  el.disabled = false;
+  el.innerText = "SelectAll-Hacked!";
+  el.click();
+  console.log('haced!');
 });
