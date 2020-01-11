2422fdd7b2c8c789d970d95aa7ac3f38dc9140b0
afeiship <1290657123@qq.com>
2020-01-11 21:46:45 +0800

    fix: el null

--- scripts/content.js
+++ scripts/content.js
@@ -1,31 +1,45 @@
+console.log("content loaded!");
+
 $(document).ready(function() {
-  var el = document.querySelector('[ng-click="gridSelectAll()"]');
-
+  console.log("ready?!");
+  var url = location.href;
+  if (!url.includes("/labor/approvalflat")) return;
   if (window.__STOP__) return;
+  var counter = 0;
+  var limit = 20;
 
-  // 1. 全选，删除 disabled 状态
-  el.disabled = false;
+  function autoinject() {
+    var el = document.querySelector('[ng-click="gridSelectAll()"]');
+    // 1. 全选，删除 disabled 状态
+    el.disabled = false;
 
-  // 2. 点击全选这个btn
-  el.click();
-  setTimeout(() => {
-    // 3. 选择 value = 2的这个选择项
-    var aprovedBtn = document.querySelector('[ng-click="ApprovalEntries()"]');
-    aprovedBtn.innerText = "ApprovalEntries-hacked!";
-    aprovedBtn.click();
-
+    // 2. 点击全选这个btn
+    el.click();
     setTimeout(() => {
-      var appReason = document.querySelector("#modLARELateApprovalReason");
-      var okBtn = document.querySelector('[ng-click="ok()"]');
-      appReason.value = "2";
-      appReason.dispatchEvent(new Event("change"));
-      appReason.appReason;
-      okBtn.click();
+      // 3. 选择 value = 2的这个选择项
+      var aprovedBtn = document.querySelector('[ng-click="ApprovalEntries()"]');
+      aprovedBtn.innerText = "ApprovalEntries-hacked!";
+      aprovedBtn.click();
 
       setTimeout(() => {
-        console.log("准备刷新啦!");
-        location.reload();
-      }, 5 * 1000);
+        var appReason = document.querySelector("#modLARELateApprovalReason");
+        var okBtn = document.querySelector('[ng-click="ok()"]');
+        appReason.value = "2";
+        appReason.dispatchEvent(new Event("change"));
+        appReason.appReason;
+        okBtn.click();
+
+        var timer = setInterval(() => {
+          console.log("准备刷新啦, counter:", limit);
+          counter++;
+          autoinject();
+          limit === 15 && clearInterval(timer);
+        }, 5 * 1000);
+      }, 1000);
     }, 1000);
-  }, 1000);
+  }
+
+  setTimeout(() => {
+    autoinject();
+  }, 3 * 1000);
 });
