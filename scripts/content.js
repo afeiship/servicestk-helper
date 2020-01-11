f1768481fecace55ed51fcf209c025c52e45ad04
afeiship <1290657123@qq.com>
2020-01-11 21:26:40 +0800

    fix: refresh

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
   var el = document.querySelector('[ng-click="gridSelectAll()"]');
+
+  if (window.__STOP__) return;
+
+  // 1. 全选，删除 disabled 状态
   el.disabled = false;
-  el.innerText = "SelectAll-Hacked!";
+
+  // 2. 点击全选这个btn
   el.click();
-  console.log('haced!');
+  setTimeout(() => {
+    // 3. 选择 value = 2的这个选择项
+    var aprovedBtn = document.querySelector('[ng-click="ApprovalEntries()"]');
+    aprovedBtn.innerText = "ApprovalEntries-hacked!";
+    aprovedBtn.click();
+
+    setTimeout(() => {
+      var appReason = document.querySelector("#modLARELateApprovalReason");
+      var okBtn = document.querySelector('[ng-click="ok()"]');
+      appReason.value = "2";
+      appReason.dispatchEvent(new Event("change"));
+      appReason.appReason;
+      okBtn.click();
+
+      setTimeout(() => {
+        console.log("准备刷新啦!");
+        location.reload();
+      }, 5 * 1000);
+    }, 1000);
+  }, 1000);
 });
